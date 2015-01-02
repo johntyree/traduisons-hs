35dbd630e11239dc8150d760e7ee9e0a8b8e8857
John Tyree <johntyree@gmail.com>
2015-01-02 01:10:41 -0600

    Move input parsing and interpretation into Client

--- executables/Main.hs
+++ executables/Main.hs
@@ -2,16 +2,12 @@
 
 module Main where
 
-import Control.Applicative
-import Control.Monad.Error
-import Control.Monad.State
-import Data.List
+import Control.Monad
 import Data.Maybe
 import System.Console.Readline (addHistory, readline)
 
-import Traduisons.API
+import Traduisons.Client
 import Traduisons.Types
-
 import qualified UI.QML as Qui
 
 main :: IO ()
@@ -54,68 +50,6 @@ readEvalPrint prompt f = do
     Just line -> do addHistory line
                     return . Just $ f line
 
-helpMsg :: String
-helpMsg = "Help yourself."
-
-runCommands :: Maybe AppState -> [Command]
-            -> IO (Either String (Maybe Message, AppState))
-runCommands appState cmds = do
-  let app = foldM (const runCommand) Nothing cmds
-  initial <- maybe (liftIO createAppState) return appState
-  runErrorT . runStateT app $ initial
-
 renderAppState :: AppState -> String
 renderAppState (AppState (Language fL) (Language tL) _ _) =
   fL ++ "|" ++ tL ++ ": "
-
-parseInput :: String -> [Command]
-parseInput ('/':s) = SwapLanguages : parseInput s
-parseInput "\EOT"  = [Exit]
-parseInput ""      = []
-parseInput ('|':s) = [SetToLanguage s]
-parseInput s
-  | "/" `isSuffixOf` s = SwapLanguages : parseInput (init s)
-  | "|" `isInfixOf` s = let (from, '|':to) = break (== '|') s
-                            f c l = [c l | not (null l)]
-                        in f SetFromLanguage from ++ f SetToLanguage to
-  | otherwise = [Translate s]
-
-createAppState :: IO AppState
-createAppState =
-  let new = return . AppState (Language "nl") (Language "en") []
-  in runErrorT newState >>= either error new
-
--- Consider representing errors as an (Error String) value as part of a
--- TranslationResult ADT.
-runCommand :: Command -> StateT AppState (ErrorT String IO) (Maybe Message)
-runCommand c =
-  modify (\a -> a { asHistory = asHistory a ++ [c] }) >> runCommand_ c
-
-runCommand_ :: Command -> StateT AppState (ErrorT String IO) (Maybe Message)
-runCommand_ SwapLanguages = get >>= \aS -> from aS >> to aS
-  where from = runCommand_ . SetFromLanguage . getLanguage . asToLang
-        to = runCommand_ . SetToLanguage . getLanguage . asFromLang
-runCommand_ (SetFromLanguage l) = const Nothing <$> modify setFromLang
-  where setFromLang appState = appState { asFromLang = Language l }
-runCommand_ (SetToLanguage l) = const Nothing <$> modify setToLang
-  where setToLang appState = appState { asToLang = Language l }
-runCommand_ (Translate rawMsg) = go (1 :: Int)
-  where
-    go n = do
-      AppState fromLang toLang _ tradState <- get
-      let message = Message fromLang rawMsg
-      translation <- liftIO . runTraduisons tradState $ translate toLang message
-      case translation of
-        Left err ->
-          if n > 0
-          then renewToken >> go 0
-          else throwError err
-        Right msg -> return $ Just msg
-runCommand_ Exit = throwError "exit"
-
-renewToken :: StateT AppState (ErrorT String IO) (Maybe a)
-renewToken = do
-  appState <- get
-  traduisonsState <- lift newState
-  put appState { asTraduisonsState = traduisonsState }
-  return Nothing
---
+++ src/Traduisons/Client.hs
@@ -0,0 +1,71 @@
+
+module Traduisons.Client where
+
+import Control.Applicative
+import Control.Monad.Error
+import Control.Monad.State
+import Data.List
+
+import Traduisons.API
+import Traduisons.Types
+
+helpMsg :: String
+helpMsg = "Help yourself."
+
+createAppState :: IO AppState
+createAppState =
+  let new = return . AppState (Language "nl") (Language "en") []
+  in runErrorT newState >>= either error new
+
+renewToken :: StateT AppState (ErrorT String IO) (Maybe a)
+renewToken = do
+  appState <- get
+  traduisonsState <- lift newState
+  put appState { asTraduisonsState = traduisonsState }
+  return Nothing
+
+parseInput :: String -> [Command]
+parseInput ('/':s) = SwapLanguages : parseInput s
+parseInput "\EOT"  = [Exit]
+parseInput ""      = []
+parseInput ('|':s) = [SetToLanguage s]
+parseInput s
+  | "/" `isSuffixOf` s = SwapLanguages : parseInput (init s)
+  | "|" `isInfixOf` s = let (from, '|':to) = break (== '|') s
+                            f c l = [c l | not (null l)]
+                        in f SetFromLanguage from ++ f SetToLanguage to
+  | otherwise = [Translate s]
+
+runCommands :: Maybe AppState -> [Command]
+            -> IO (Either String (Maybe Message, AppState))
+runCommands appState cmds = do
+  let app = foldM (const runCommand) Nothing cmds
+  initial <- maybe (liftIO createAppState) return appState
+  runErrorT . runStateT app $ initial
+
+-- Consider representing errors as an (Error String) value as part of a
+-- TranslationResult ADT.
+runCommand :: Command -> StateT AppState (ErrorT String IO) (Maybe Message)
+runCommand c =
+  modify (\a -> a { asHistory = asHistory a ++ [c] }) >> runCommand' c
+runCommand' :: Command -> StateT AppState (ErrorT String IO) (Maybe Message)
+runCommand' SwapLanguages = get >>= \aS -> from aS >> to aS
+  where from = runCommand' . SetFromLanguage . getLanguage . asToLang
+        to = runCommand' . SetToLanguage . getLanguage . asFromLang
+runCommand' (SetFromLanguage l) = const Nothing <$> modify setFromLang
+  where setFromLang appState = appState { asFromLang = Language l }
+runCommand' (SetToLanguage l) = const Nothing <$> modify setToLang
+  where setToLang appState = appState { asToLang = Language l }
+runCommand' (Translate rawMsg) = go (1 :: Int)
+  where
+    go n = do
+      AppState fromLang toLang _ tradState <- get
+      let message = Message fromLang rawMsg
+      translation <- liftIO . runTraduisons tradState $ translate toLang message
+      case translation of
+        Left err ->
+          if n > 0
+          then renewToken >> go 0
+          else throwError err
+        Right msg -> return $ Just msg
+runCommand' Exit = throwError "exit"
