967c495b17d089f569c725af07fdf32cd7f060ad
John Tyree <johntyree@gmail.com>
2015-09-20 21:22:38 -0500

    Use nativeCurl function for all requests.
    Previously we launched a curl subprocess because Haskell's
    TLS implementation did not play nicely with some servers.

--- src/Traduisons/Util.hs
+++ src/Traduisons/Util.hs
@@ -8,8 +8,6 @@ import Data.Maybe
 import Data.Time.Clock.POSIX
 import Network.HTTP.Client as N
 import Network.HTTP.Types
-import System.Process
-import System.Exit
 import Control.Exception
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy.Char8 as BL
@@ -33,18 +31,13 @@ liftEither = either throwError return
 currentTime :: IO Seconds
 currentTime = fmap round getPOSIXTime
 
-curl :: (MonadIO m, Functor m) => URL -> StdMethod -> [Header] -> FormData
-      -> Manager -> ExceptT TraduisonsError m B.ByteString
-curl url = if ssl then systemCurl url else nativeCurl url
-  where ssl = "https" `isPrefixOf` url
-
 mkReq :: String -> Either TraduisonsError Request
 mkReq url = let err = Left $ TErr CurlError ("Failed to parse URL: " ++ show url)
             in maybe err Right (parseUrl url)
 
-nativeCurl :: MonadIO m => URL -> StdMethod -> [Header] -> FormData
-           -> Manager -> ExceptT TraduisonsError m B.ByteString
-nativeCurl url httpMethod hdrs formData man = do
+curl :: (MonadIO m, Functor m) => URL -> StdMethod -> [Header] -> FormData
+      -> Manager -> ExceptT TraduisonsError m B.ByteString
+curl url httpMethod hdrs formData man = do
   req'' <- liftEither (mkReq url)
   req' <- case httpMethod of
             -- FIXME: Make a newtype for this
@@ -65,28 +58,6 @@ nativeCurl url httpMethod hdrs formData man = do
   case resp of
     Left err -> throwError $ TErr CurlError (show err)
     Right body -> return . BL.toStrict . responseBody $ body
-
--- Working around broken TLS (https://github.com/vincenthz/hs-tls/issues/87)
-systemCurl :: (MonadIO m, Functor m) => URL -> StdMethod -> [Header]
-           -> FormData -> Manager -> ExceptT TraduisonsError m B.ByteString
-systemCurl url httpMethod hdrs formData _ = do
-  let dataAsArg = [B.concat [a, "=", b] | (a, Just b) <- formData]
-      addFlag flag argList = flag : intersperse flag argList
-      -- It would be nicer here to use something to just drop the case
-      -- insensistivity and keep the ByteString, but whatever
-      hdr (a, b) = B.concat [read (show a), ":", b]
-      argHdrs = addFlag "-H" $ map hdr hdrs
-  argData <- case httpMethod of
-              GET -> return $ "-G" : addFlag "-d" dataAsArg
-              POST -> return $ addFlag "--data-urlencode" dataAsArg
-              _ -> let err = "Curl doesn't know " ++ show httpMethod
-                   in throwError $ TErr CurlError err
-  let args = "-s" : "-S" : url : map B.unpack (argHdrs ++ argData)
-  curlResult <- liftIO (readProcessWithExitCode "curl" args "")
-  case curlResult of
-    (ExitSuccess, stdout, _) -> return (B.pack stdout)
-    (ExitFailure _, _, stderr) -> throwError $ TErr CurlError stderr
-
 
 -- Remove the BOM from Unicode string
 stripBOM :: String -> String
